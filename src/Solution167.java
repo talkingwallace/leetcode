eb21582d489e355dba3310a2472c560cd76ae7f6
Chen <cwjghglbdcj@gmail.com>
2019-01-28 21:05:28 +0800

    2019/1/28

---
+++ src/Solution167.java
@@ -0,0 +1,13 @@
+public class Solution167 {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        if(numbers.length==0)return null;
+        int result[] = new int[2];
+        int count[] = new int[numbers[numbers.length-1]];
+
+
+        return result;
+
+    }
+}
