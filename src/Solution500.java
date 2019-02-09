897451105ca40702e1d56578e0363cdddc492989
Chen <cwjghglbdcj@gmail.com>
2019-02-09 19:18:46 +0800

    2019/2/9

---
+++ src/Solution500.java
@@ -0,0 +1,36 @@
+public class Solution500 {
+    // only python3 version 
+    /*
+    * class Solution(object):
+
+    def inRow(self,word):
+        word = word.lower()
+        row1 ="qwertyuiop";
+        row2 ="asdfghjkkl";
+        row3 ="zxcvbnm";
+        test = ""
+        if word[0] in row1:
+            test = row1
+        elif word[0] in row2:
+            test = row2
+        elif word[0] in row3:
+            test = row3
+
+        for i in word:
+            if not i in test:
+                return False
+        return True
+
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        rs = []
+        for i in words:
+            if self.inRow(i):
+                rs.append(i)
+
+        return rs
+    * */
+}
--- src/Solution836.java
+++ src/Solution836.java
@@ -1,9 +1,19 @@
 public class Solution836 {
 
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return !(rec1[2] <= rec2[0] ||   // left
-                rec1[3] <= rec2[1] ||   // bottom
-                rec1[0] >= rec2[2] ||   // right
-                rec1[1] >= rec2[3]);    // top
+
+    //这个完全搞不懂 智商不够用
+//    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+//        return !(rec1[2] <= rec2[0] ||   // left
+//                rec1[3] <= rec2[1] ||   // bottom
+//                rec1[0] >= rec2[2] ||   // right
+//                rec1[1] >= rec2[3]);    // top
+//    }
+
+    // another solution using area computation
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2){
+        int width = Math.min(rec1[2],rec2[2]) - Math.max(rec1[0],rec2[0]);
+        int height = Math.min(rec1[3],rec2[3]) - Math.max(rec1[1],rec2[1]);
+        if(width>0&&height>0)return true;
+        else return false;
     }
 }
